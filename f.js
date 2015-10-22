9428b58edae2d7e8d7337e3025d59831cbd1e113
yxch <yxch@live.com>
2015-10-22 10:13:26 +0800

    Update f.js

--- f.js
+++ f.js
@@ -11,6 +11,59 @@
 1.先定义一个构造函数，并设置初始化新对象的实例属性。
 2.给构造函数的prototype对象定义实例的方法
 3.给构造定义类字段和类属性
+比如定义一个复数类，像下面这个样式的：
+	function Complex(real,imaginary)
+        {
+            if(isNaN(real) || isNaN(imaginary)) throw new TypeError();
+            this.r = real;
+            this.i = imaginary;
+        }
+        Complex.prototype.add = function(that)
+        {
+            return new Complex(this.r + that.r,this.i + that.i);
+        };
+        Complex.prototype.mul = function(that)
+        {
+            return new Complex(this.r * that.r - this.i * that.i,this.r * that.i + this.i * that.r);
+        };
+        Complex.prototype.mag = function(that)
+        {
+            return Math.sqrt(this.r * this.r + this.i * this.i);
+        };
+        Complex.prototype.neg = function()
+        {
+            return new Complex(-this.r,-this.i);
+        };
+        Complex.prototype.toString = function()
+        {
+            return "{" + this.r + "," + this.i + "}";
+        };
+        Complex.prototype.equals = function(that)
+        {
+            return that != null && that.constructor === Complex && this.r === that.r && this.i === that.i;
+        };
+        Complex.ZERO = new Complex(0,0);
+        Complex.ONE = new Complex(1,0);
+        Complex.I = new Complex(0,1);
+
+        Complex.parse = function(s)
+        {
+            try
+            {
+                var m = Complex._format.exec(s);
+                return new Complex(parseFloat(m[1]),parseFloat((m[2])));
+            }catch(x)
+            {
+                throw new TypeError("Can't parse'" + s + "' as a complex number.");
+            }
+        };
+        Complex._format = /^{([^,]+),([^}]+)\}$/;
+
+        var c = new Complex(2,3);
+        var d = new Complex(c.i,c.r);
+        alert(c.add(d).toString());
+        alert(Complex.parse(c.toString()).add(c.neg()).equals(Complex.ZERO));
+
 
 /*--------------------可选函数----------------------------*/
 function counter(n){
